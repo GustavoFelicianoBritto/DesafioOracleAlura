8fc9e449b4e69c821fede9cd557a7e48520c0c65
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-08-13 19:58:31 -0300

    Função adicionar nome no array e exibir em UL com append de novo IL

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosLista=[];
+const htmlListaNome = document.getElementById('listaAmigos');
+const itensListaNome = document.createElement('li');
+
+function adicionarAmigo()
+    {
+        let amigoNome = document.querySelector('input').value
+        amigosLista.push(amigoNome);
+        const itensListaNome = document.createElement('li');
+        itensListaNome.textContent=amigoNome;
+        htmlListaNome.appendChild(itensListaNome);
+        console.log(amigosLista);
+    }
