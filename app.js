2e846eee89cb17f56f43cab172eef90aeeca3765
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-08-13 20:28:11 -0300

    Sorteio, apresentação na tela e alternar entre hidden e not hidden

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 let amigosLista=[];
 const htmlListaNome = document.getElementById('listaAmigos');
 const itensListaNome = document.createElement('li');
+const resultado = document.getElementById('resultado');
 
 function adicionarAmigo()
     {
         let amigoNome = document.querySelector('input').value
-        amigosLista.push(amigoNome);
-        const itensListaNome = document.createElement('li');
-        itensListaNome.textContent=amigoNome;
-        htmlListaNome.appendChild(itensListaNome);
-        console.log(amigosLista);
+        if(amigoNome.trim() !== '')
+            {
+                htmlListaNome.removeAttribute('hidden');
+                amigoNome = amigoNome.trim();
+                amigosLista.push(amigoNome);
+                const itensListaNome = document.createElement('li');
+                itensListaNome.textContent=amigoNome;
+                htmlListaNome.appendChild(itensListaNome);
+                console.log(amigosLista);
+            }
+            else
+                {
+                    alert("Campo vazio!");
+                }
+    }
+
+
+function sortearAmigo()
+    {
+        if(amigosLista.length>0)
+        {
+            let numero = parseInt(Math.random() * amigosLista.length);
+            console.log(numero);
+            console.log(amigosLista[numero]);
+            htmlListaNome.setAttribute('hidden','');
+            resultado.innerHTML="Amigo sorteado: "+amigosLista[numero]+""
+        }
+        
+
+        
     }
